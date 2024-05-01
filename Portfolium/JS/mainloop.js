71b4afb154c5c8bb013434e298a745479275ac15
FAMohrCode <166504250+FAMohrCode@users.noreply.github.com>
2024-05-01 19:53:32 +0200

    Alpha v. 0.01.04

---
+++ Portfolium/JS/mainloop.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    document.getElementById("Python").onclick = function() {
+        window.location.href = 'python.html';
+    };
+
+    document.getElementById("JavaScript").onclick = function() {
+        window.location.href = 'js.html';
+    };
+
+    document.getElementById("HTML").onclick = function() {
+        window.location.href = 'html.html';
+    };
+
+    document.getElementById("CSS").onclick = function() {
+        window.location.href = 'css.html';
+    };
+
+    document.getElementById("PHP").onclick = function() {
+        window.location.href = 'php.html';
+    };
+
+    document.getElementById("Java").onclick = function() {
+        window.location.href = 'java.html';
+    };
+
+    document.getElementById("C").onclick = function() {
+        window.location.href = 'c.html';
+    };
+};
