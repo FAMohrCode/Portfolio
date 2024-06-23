61271828c28761f027dd38d6ff14f5bd4459ad46
FAMohrCode <166504250+FAMohrCode@users.noreply.github.com>
2024-06-23 19:51:13 +0200

    Alpha 1.2.6

--- BlogWebsite/JS.js
+++ BlogWebsite/JS.js
@@ -34,29 +34,56 @@ setInterval(Main, 500);
 function footerText() {
     const footerHTML = `
         <footer class="bg-slate-900 rounded-lg flex flex-col items-center justify-center py-8 gap-4 mb-0 mt-10">
-            <h3>
-                <b>Credits & License:</b>
-            </h3>
+            <h3><b>Credits & License:</b></h3>
             <div class="flex flex-col items-center justify-center py-8 gap-2">
                 <div class="flex flex-col items-center justify-center">
-                    <p>Create by FAMohr / Felix Adrian Mohr:</p>
-                    <p>on Github: <span class="underline text-yellow-300"><a href="https://github.com/FAMohrCode" target="_blank">FAMohrCode</a></span></p>
-                    <p>on YouTube: <span class="underline text-yellow-300"><a href="https://www.youtube.com/channel/UCfVrPzqPAE0sBpKRp6W4xwg" target="_blank">@TheFAMohr / FAMohr</a></span></p>
-                    <p>on Discord: <span class="text-yellow-300">famohr</span></p>
+                    <p class="Credit">Create by FAMohr / Felix Adrian Mohr:</p>
+                    <p class="Credit">on Github: <span class="underline text-yellow-300 footerData"><a href="https://github.com/FAMohrCode" target="_blank">FAMohrCode</a></span></p>
+                    <p class="Credit">on YouTube: <span class="underline text-yellow-300 footerData"><a href="https://www.youtube.com/channel/UCfVrPzqPAE0sBpKRp6W4xwg" target="_blank">@TheFAMohr / FAMohr</a></span></p>
+                    <p class="Credit">on Discord: <span class="text-yellow-300 footerData">famohr</span></p>
                 </div>
                 <div>
-                    <p>Begin of this Project: <span class="text-yellow-300">2024-06-20</span></p>
-                    <p>Last Update: <span class="text-yellow-300">2024-06-23_13:04</span></p>
-                    <p>Version: <span class="text-yellow-300">Alpha 1.2.2</span></p>
+                    <p class="Credit">Begin of this Project: <span class="text-yellow-300 footerData">2024-06-20</span></p>
+                    <p class="Credit">Last Update: <span class="text-yellow-300 footerData">2024-06-23_19:49</span></p>
+                    <p class="Credit">Version: <span class="text-yellow-300 footerData">Alpha 1.2.6</span></p>
                 </div>
             </div>
             <div>
                 <div class="flex flex-col items-center justify-center">
-                    <p>Create in: <span class="underline text-yellow-300"><a href="https://code.visualstudio.com/" target="_blank">Visual Studio Code</a></span></p>
+                    <p class="Credit">Create in: <span class="underline text-yellow-300 footerData"><a href="https://code.visualstudio.com/" target="_blank">Visual Studio Code</a></span></p>
+                    <p class="Credit">Picture from: <span class="underline text-yellow-300 footerData"><a href="https://pixabay.com/" target="_blank">Pixabay</a></span></p>
                 </div>
             </div>
         </footer>
     `;
 
     document.body.insertAdjacentHTML('beforeend', footerHTML);
+}
+
+function randomBanner() {
+    var max = 5;
+    var number = Math.floor(Math.random() * max);
+    var bannerSrc;
+
+    switch (number) {
+        case 0:
+            bannerSrc = "/src/IMG/Banner1.jpg";
+            break;
+        case 1:
+            bannerSrc = "/src/IMG/Banner2.jpg";
+            break;
+        case 2:
+            bannerSrc = "/src/IMG/Banner3.jpg";
+            break;
+        case 3:
+            bannerSrc = "/src/IMG/Banner4.jpg";
+            break;
+        case 4:
+            bannerSrc = "/src/IMG/Banner5.png";
+            break;
+        default:
+            bannerSrc = "/src/IMG/Banner1.jpg";
+    }
+
+    document.getElementById("banner").src = bannerSrc;
 }
